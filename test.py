3f9dfcfa5b1594db12404f3be361b7edf35c1f8c
unknown <I844796@SLEN00519151A.amer.global.corp.sap>
2015-02-02 17:20:24 -0200

    changed the file to support read from a json file

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
-import serial
+#import serial
 import requests
 import json
 
 #url = 'https://smartofficei837119trial.hanatrial.ondemand.com/rest'
-url = 'http://10.2.137.129:8080/SmartOffice/rest'
+url = 'http://10.2.124.227:8080/SmartOffice/rest'
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+#ser = serial.Serial('/dev/ttyACM0', 9600)
 
 while 1 :
-    readStr = ser.readline()
-    print readStr
+#    readStr = ser.readline()
+    readStr = '500|400|400|24.3'
+    print (readStr)
     readArr = readStr.split('|')
     bathroom = int(readArr[0]) < 300
     parkingLot = int(readArr[2]) > 300
@@ -17,3 +18,9 @@ while 1 :
     requests.put(url + '/bathroom/2EM/' + str(bathroom), headers=headers)
     requests.put(url + '/noiseSensor/1/' + readArr[1], headers=headers)
     requests.put(url + '/parkingLot/vaga1/' + str(parkingLot), headers=headers)
+    response = requests.get(url + '/lightSensor', headers=headers)
+    json_data = response.json()
+    
+    
+    #json.loads(response.raw.decode())
+    print (json_data[0]['level'])    
